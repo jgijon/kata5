52287a91c75c4ba35a11b7c51568dffc017da52c
usuario <usuario@10.22.146.199>
2013-11-15 14:14:46 +0000

    Primer commit kata5

---
+++ src/kata5/AttributeExtractor.java
@@ -0,0 +1,7 @@
+package kata5;
+
+public interface AttributeExtractor<E, A> {
+    
+    public A extract(E entity);
+    
+}
---
+++ src/kata5/Kata5.java
@@ -0,0 +1,17 @@
+package kata5;
+
+public class Kata5 {
+
+    public static void main(String[] args) {
+        MailLoader loader = new MockMailLoader();
+        HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
+        ChartHistogramViewer<String> viewer = new ChartHistogramViewer<>();
+        viewer.show(builder.build(new AttributeExtractor<Mail, String>() {
+
+            @Override
+            public String extract(Mail entity) {
+                return entity.getDomain();
+            }
+        }));
+    }
+}
